ee0be8fcfc238e4dea42fb796e61d0feaa7c6e49
rayct <rayct@icloud.com>
2021-06-16 01:18:32 +0100

    navbar buton styling and icon addition

--- fcc-scratchpad.js
+++ fcc-scratchpad.js
@@ -1,4 +1,11 @@
+function isLess(a, b) {
+    // Only change code below this line
+    return a === b; {
+        return true;
+        // Only change code above this line
+    }
 
+    isLess(10, 15);
 
 
 
