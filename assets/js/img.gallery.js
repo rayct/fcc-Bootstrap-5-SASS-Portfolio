655c6c99ddb5d0a6d8ef087802e42e9385912505
rayct <rayct@icloud.com>
2021-07-01 01:34:20 +0100

    Image Gallery Update

--- assets/js/img.gallery.js
+++ assets/js/img.gallery.js
@@ -1,1 +1,13 @@
 // IMAGE MODAL EVEN LISTENER
+document.addEventListener("click", function (e) {
+    console.log(e.target)
+    if (e.target.classsList.contains("gallery-item")) {
+        const src = e.target.getAttribute("src");
+        console.log(src);
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show();
+
+        
+    }
+})
