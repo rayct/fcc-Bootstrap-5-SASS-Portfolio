c867a98b35d8c843b43a0ceb447cd6532dee22b4
rayct <rayct@icloud.com>
2021-07-01 02:07:04 +0100

    Ammended img.gallery.js

--- assets/js/img.gallery.js
+++ assets/js/img.gallery.js
@@ -1,7 +1,7 @@
 // IMAGE MODAL EVEN LISTENER
 document.addEventListener("click", function (e) {
     console.log(e.target)
-    if (e.target.classsList.contains("gallery-item")) {
+    if (e.target.classsList.contains("img-fluid")) {
         const src = e.target.getAttribute("src");
         console.log(src);
         document.querySelector(".modal-img").src = src;
